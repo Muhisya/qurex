8aa5df431a305bd079d0820f3a6f0ec1bb4ecb3e
Muhammad Hibban Syakir <144977923+Muhisya@users.noreply.github.com>
2026-04-17 09:06:14 +0700

    deploying

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
